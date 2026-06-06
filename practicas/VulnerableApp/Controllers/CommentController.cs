e463ffa6a7012cdc222ad802f4b1e6d1bc79bc3d
Ricardo Reséndiz González <2024171007@uteq.edu.mx>
2026-06-05 20:31:12 -0600

    avance practica P2D P2E

---
+++ practicas/VulnerableApp/Controllers/CommentController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace VulnerableApp.Controllers
+{
+    public class CommentController : Controller
+    {
+        private static List<string> _comments = new();
+
+        public IActionResult Index()
+        {
+            return View(_comments);
+        }
+
+        [HttpPost]
+        public IActionResult AddComment(string comment)
+        {
+            if (!string.IsNullOrEmpty(comment))
+            {
+                _comments.Add(comment);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
